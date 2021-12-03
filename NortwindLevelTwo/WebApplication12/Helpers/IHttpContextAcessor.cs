80254bf170281f9094623f4694daa23ea8277fcc
BekirDelikucuk <91602566+BekirDelikucuk@users.noreply.github.com>
2021-12-03 22:32:56 +0300

    ASP.NET CORE 3.1 Northwind Demo
    Object-oriented program using with entity framework

---
+++ NortwindLevelTwo/WebApplication12/Helpers/IHttpContextAcessor.cs
@@ -0,0 +1,6 @@
+﻿namespace MvcWebUI.Helpers
+{
+    internal interface IHttpContextAcessor
+    {
+    }
+}
---
+++ NortwindLevelTwo/WebApplication12/ValidationRules/FluentValidation/ShippingDetailValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using MvcWebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcWebUI.ValidationRules.FluentValidation
+{
+    public class ShippingDetailValidator : AbstractValidator<ShippingDetail>
+    {
+        public ShippingDetailValidator()
+        {
+            RuleFor(expression: s => s.FirstName).NotEmpty().WithMessage("İsim gerekli");
+            RuleFor(expression: s => s.FirstName).MinimumLength(2);
+            RuleFor(expression: s => s.LastName).NotEmpty();
+            RuleFor(expression: s => s.City).NotEmpty();
+            RuleFor(expression: s => s.Email).NotEmpty();
+        }
+    }
+}
